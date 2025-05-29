6a40390a2425638b21bbe50809acbeb4036931ee
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-05-29 09:10:21 +0500

    add models/TableData

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     "drf_yasg",
+    "django_filters",
 
     # Локальные приложения
     "table_api",
@@ -127,3 +128,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
+}
--- backend/table_api/models.py
+++ backend/table_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class TableData(models.Model):
+    """Модель для хранения данных таблицы"""
+    date = models.DateField(default=timezone.now, verbose_name="Дата")
+    name = models.CharField(max_length=255, verbose_name="Название")
+    quantity = models.PositiveIntegerField(verbose_name="Количество")
+    distance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Расстояние")
+
+    class Meta:
+        verbose_name = "Данные таблицы"
+        verbose_name_plural = "Данные таблицы"
+        ordering = ['-id']
+
+    def __str__(self):
+        return f"{self.name} ({self.date})"
